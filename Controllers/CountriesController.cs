f2fd91b131ce25d198c8eda3c278f24d97392994
Abhijit Desai <abhijitno1@yahoo.com>
2023-08-03 17:19:56 -0400

    correction made to make project runnable on IIS

--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ASPnetCoreReact.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("[controller]")]
     public class CountriesController : ControllerBase
